58b4e0db9b4b86f1f51559c8a9df900a760008f0
Nicolas Aguilar <61167557+nicktify@users.noreply.github.com>
2024-03-17 16:11:51 -0300

    Fix issues with multiple files configuration

--- init.lua
+++ init.lua
@@ -1,23 +1,15 @@
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-require('lua/_packer')
-require('lua/_options')
-require('lua/_lsp')
-require('lua/_git')
-require('lua/_lualine')
-require('lua/_bufferline')
-require('lua/_formatter')
-require('lua/_configuration')
-require('lua/_keymap')
-require('lua/_dap')
-require('lua/_telescope')
-require('lua/_treesitter')
+require('_keymap')
+require('_configuration')
+require('_packer')
+require('_options')
+require('_lsp')
+require('_git')
+require('_lualine')
+require('_bufferline')
+require('_formatter')
+require('_dap')
+require('_telescope')
+require('_treesitter')
 
 require('Comment').setup()
 require('go').setup()
--- lua/_configuration.lua
+++ lua/_configuration.lua
@@ -1,3 +1,7 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
@@ -10,6 +14,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.g.gitblame_display_virtual_text = 0
-
-
 vim.opt.termguicolors = true
+
